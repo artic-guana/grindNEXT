ee04e54e13f68b448700ebaa7d94ffe51bd6d61d
Rishu <suryodipta@gmail.com>
2026-09-13 12:22:18 +0530

    update auth

--- Backend/src/services/auth.service.js
+++ Backend/src/services/auth.service.js
@@ -1,10 +1,12 @@
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
 import { nativeDb } from "../config/db.js";
 import { env } from "../config/env.js";
 
 export const auth = betterAuth({
   baseURL: env.BETTER_AUTH_URL,
+
   secret: env.BETTER_AUTH_SECRET,
 
   database: mongodbAdapter(nativeDb),
@@ -13,11 +15,22 @@ export const auth = betterAuth({
     enabled: true,
   },
 
-  trustedOrigins: [env.CLIENT_URL],
+  trustedOrigins: [
+    env.CLIENT_URL,
+    env.CLIENT_URL2,
+  ].filter(Boolean),
 
   advanced: {
     database: {
       joins: false,
+    },
+
+    useSecureCookies: true,
+
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
     },
   },
 });
